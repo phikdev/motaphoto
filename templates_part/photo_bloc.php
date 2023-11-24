00a64647fd144d3cf8293ac36edd6eee6aa68fa2
phik <phik.dev@gmail.com>
2023-11-24 16:49:07 +0100

    correction photo apparenté

--- templates_part/photo_bloc.php
+++ templates_part/photo_bloc.php
@@ -1,25 +1,36 @@
 
-<?php 
-    $current_category = get_the_terms(get_the_ID(), 'categorie');
-    $the_query = new WP_Query(array(
-        'orderby' => 'rand',
-        'post_type' => 'photo',
-        'posts_per_page' => 2,
-        'tax_query' => array(
-            array(
-                // Prendre les images de la taxonomie de "catégorie"
-                'taxonomy' => 'categorie',
-                // Prendre l'ID de la photo affichée sur le single.php
-                'field' => 'term_id',
-                'terms' => $current_category[0]->term_id, // Utilisez la catégorie actuelle
-            ),
-        ),
-    ));?>
+<?php
+                // Récupère la catégorie de la photo en cours
+                $terms = get_the_terms(get_the_ID(), 'categorie');
+                if ($terms && !is_wp_error($terms) && is_array($terms) && !empty($terms)) {
+                    $first_category = $terms[0]->slug;
+                } else {
+                    // Gère le cas où aucune catégorie n'est trouvée.
+                    $first_category = 'categorie-par-defaut'; // Remplacez par une valeur par défaut ou laissez vide selon vos besoins.
+                }
+
+                // Requête pour récupérer jusqu'à deux photos du même type de catégorie que la photo actuelle
+                $args = array(
+                    'post_type' => 'photo',
+                    'post__not_in' => array(get_the_ID()),
+                    'posts_per_page' => 2,
+                    'tax_query' => array(
+                        array(
+                            'taxonomy' => 'categorie',
+                            'field'    => 'slug',
+                            'terms'    => $first_category,
+                        ),
+                    ),
+                );
+
+                $query = new WP_Query($args);
+                
+            ?>
     <div id="photo-galerie">
         <?php
-    if($the_query->have_posts()):
-    while ($the_query -> have_posts()) {
-        $the_query -> the_post();
+    if($query->have_posts()):
+    while ($query -> have_posts()) {
+        $query -> the_post();
         
            the_post_thumbnail();
         
