8b81d0ae0df2bf3ec3a7c82bb4c66ad1a23b9f28
phik <phik.dev@gmail.com>
2024-04-26 01:51:37 +0200

    Responsive final

--- templates_part/modale.php
+++ templates_part/modale.php
@@ -1,17 +1,11 @@
+<!--  Modal -->
 
-
-
-<!-- The Modal -->
 <div id="myModal" class="modal">
 
-  <!-- Modal content -->
-  <div class="modal-content">
-  <div id="contact"></div>
-  <?php echo do_shortcode( '[contact-form-7 id="78c5ee7" title="Formulaire de contact 1"]');
-     ?>
-    
-    
-    
-  </div>
-
+    <!-- Modal content -->
+    <div class="modal-content">
+      <div id="contact"></div>
+        <?php echo do_shortcode( '[contact-form-7 id="78c5ee7" title="Formulaire de contact 1"]');
+          ?>
+    </div>
 </div>
